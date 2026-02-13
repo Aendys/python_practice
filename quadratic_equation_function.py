e227aa1995b87a35b09987da000581abee742895
andrea.horavova <andrea.horavova@tfbank.se>
2026-02-13 12:24:40 +0100

    Added changes to quadratic_function

--- quadratic_equation_function.py
+++ quadratic_equation_function.py
@@ -1,32 +1,39 @@
 import cmath
 
-#Text introduction
-print("Hello user, this is a program for calculating a quadratic equation. Insert a values for variables A,B and C to calculate.")
+def input_data():
+    print("Dear user, this program calculates a quadratic equation. Insert a values for variables A,B and C to calculate.")
+    a = input("Please enter a number for variable a: ")
+    b = input("Please enter a number for variable b: ")
+    c = input("Please enter a number for variable c: ")
+    return a, b, c
 
-#Ask for parameter inputs A,B,C
-a = float(input("Please enter a number for variable A:"))
-b = float(input("Please enter a number for variable B:"))
-c = float(input("Please enter a number for variable C:"))
 
-#Print equation Ax2 + Bx + C = 0
-print("Calculating ",  a,"x2 + ", b,"x + ", c , "= 0:")
+def quadratic_calculation(a, b, c):
+    a, b, c = float(a), float(b), float (c)
 
-# Body with the program to calculate the equation
-def quadratic(a, b, c):
-    d = (b**2) - (4*a*c) #discrimininat
-
-    if d > 0:
-        x1 = (- b - cmath.sqrt(d)) / (2*a)
-        x2 = (- b + cmath.sqrt(d)) / (2*a)
-        print("The equation has 2 solutions: {0} and {1}".format(x1, x2))
-    elif d == 0:
-        x = (- b + cmath.sqrt(d)) / (2*a)
-        print ("The equations has 1 solution: {0} ".format(x))
+    if a == 0:
+        print("Cannot be divided by zero.")
     else:
-        print ("The equations has 0 solutions.")
+        print("Calculating ", a, "x2 + ", b, "x + ", c, "= 0 ...")
+        d = (b ** 2) - (4* a *c ) #discrimininat
+
+        if d > 0:
+            x1 = (- b - cmath.sqrt(d)) / (2 * a)
+            x2 = (- b + cmath.sqrt(d)) / (2 * a)
+            print(f"The equation has 2 solutions: {x1} and {x2}.")
+
+        elif d == 0:
+            x = (- b + cmath.sqrt(d)) / (2 * a)
+            print (f"The equations has 1 solution: {x}.")
+        else:
+            print ("The equations has 0 solutions.")
+
+def output():
+    a, b, c = input_data()
+    if a and b and c is not None:
+        quadratic_calculation(a, b, c)
+    else:
+        print("Please enter valid data.")
 
 
-if a == 0:
-    print("Variable a cannot be 0. Enter correct value.")
-else:
-    quadratic(a, b, c)
+output()
